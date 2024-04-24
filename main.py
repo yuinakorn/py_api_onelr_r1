298504e8e0046b11c98e272fe3666cee110d2830
yuinakorn <yuinakorn@hotmail.com>
2024-04-24 21:57:40 +0700

    allow origin

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dotenv import dotenv_values
 from fastapi import FastAPI
 
 from routers import pregs_router, infants_router, dashboard_router, progress_router, auth_router
@@ -6,16 +7,28 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # app = FastAPI(docs_url=None, redoc_url=None)
 app = FastAPI(docs_url="/api-docs", redoc_url=None)
+config_env = dotenv_values(".env")
 
 # Allow requests from all origins
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+origins = [
+    config_env["CORS_ORIGIN1"]
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.on_event("startup")
 async def startup_event():
